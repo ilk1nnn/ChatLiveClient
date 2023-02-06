b05fd2d5bc6536cf854e3e089e291d135b748255
ilkin <ilkin@MY_ASUS>
2023-02-06 18:58:16 +0400

    Add project files.

---
+++ WPF_ClientTcp/ViewModels/MainViewModel.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using ChatLiveClient.Commands;
+using ChatLiveClient.Services.NetworkService;
+using WPF_ClientTcp;
+
+namespace ChatLiveClient.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Properties
+
+        public TcpClient TcpClient { get; set; }
+
+
+        private string clientMessage;
+
+        public string ClientMessage
+        {
+            get { return clientMessage; }
+            set { clientMessage = value; OnPropertyChanged(); }
+        }
+
+
+        private string clientName;
+
+        public string ClientName
+        {
+            get { return clientName; }
+            set { clientName = value; OnPropertyChanged(); }
+        }
+
+
+        private string displayName;
+
+        public string DisplayName
+        {
+            get { return displayName; }
+            set { displayName = value; OnPropertyChanged(); }
+        }
+
+
+        public BinaryReader BinaryReader { get; set; }
+
+        public BinaryWriter BinaryWriter { get; set; }
+
+        public bool IsConnected { get; set; }
+
+
+        private string connectContent;
+
+        public string ConnectContent
+        {
+            get { return connectContent; }
+            set { connectContent = value; OnPropertyChanged(); }
+        }
+
+        public StackPanel MessagePanel { get; set; }
+
+        #endregion
+
+        ///---------------------------------------------------------------
+
+        #region Commands
+        public RelayCommand ConnectCommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+        public RelayCommand DisconnectCommand { get; set; }
+
+
+        #endregion
+
+        public DispatcherTimer messageReceiverTimer { get; set; }
+
+
+
+
+        public async void ReceiveMessage()
+        {
+            messageReceiverTimer.Stop();
+            await Task.Run(() =>
+            {
+                App.Current.Dispatcher.Invoke((Action)delegate
+                {
+
+                    try
+                    {
+                        var stream = TcpClient.GetStream();
+                        BinaryReader = new BinaryReader(stream);
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+
+                });
+            });
+        }
+
+
+
+        public MainViewModel()
+        {
+            ConnectContent = "Connect";
+            TcpClient = new TcpClient();
+            var ip = IPAddress.Parse(IPService.GetLocalIPAddress());
+            var port = 27001;
+            var endPoint = new IPEndPoint(ip, port);
+
+            messageReceiverTimer = new DispatcherTimer();
+            messageReceiverTimer.Interval = TimeSpan.FromSeconds(3);
+            messageReceiverTimer.Tick += MessageReceiverTimer_Tick;
+            ConnectCommand = new RelayCommand((c) =>
+            {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        TcpClient.Connect(endPoint);
+                        var stream = TcpClient.GetStream();
+                        BinaryWriter = new BinaryWriter(stream);
+                        BinaryWriter.Write(ClientName);
+                        IsConnected = true;
+                        messageReceiverTimer.Start();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("You are already connected or connection was not sucessfull");
+                        ClientName = "";
+                    }
+                });
+            }, (a) =>
+            {
+                if (!IsConnected)
+                {
+                    if (ClientName != null)
+                    {
+                        if (ClientName.Length != 0)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                    return false;
+                }
+                else if (IsConnected)
+                    return false;
+                return false;
+            });
+
+
+
+            SendCommand = new RelayCommand((c) =>
+            {
+                if (TcpClient.Connected)
+                {
+                    App.Current.Dispatcher.Invoke((Action)delegate // <--- HERE
+                    {
+                        var stream = TcpClient.GetStream();
+                        BinaryWriter = new BinaryWriter(stream);
+                        BinaryWriter.Write(ClientMessage);
+                    });
+
+
+                }
+            });
+        }
+
+        private void MessageReceiverTimer_Tick(object sender, EventArgs e)
+        {
+            ReceiveMessage();
+        }
+    }
+}
---
+++ WPF_ClientTcp/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ChatLiveClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatLiveClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new MainViewModel();
+            //viewModel.MessagePanel = this.messagePanel;
+            this.DataContext = viewModel;
+        }
+    }
+}
